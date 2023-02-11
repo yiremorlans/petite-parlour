f69e37de7d560afb6e5c477ff9b813888c161017
Yire Morlans <yire.m@icloud.com>
2023-02-11 14:40:02 -0500

    refactor: add link to stylesheet as per express-react-views

--- views/Admin.jsx
+++ views/Admin.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
-// import './admin.css';
 
 function Admin(props) {
   const { requests } = props;
   return (
-    <div>
-      <h1>Service Requests</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Time</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Service</th>
-            <th>Notes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {requests.map((request) => (
-            <tr key={request._id}>
-              <td>{request.date}</td>
-              <td>{request.time}</td>
-              <td>{request.name}</td>
-              <td>{request.email}</td>
-              <td>{request.phone}</td>
-              <td>{request.service}</td>
-              <td>{request.notes}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+     <head>
+         <link rel="stylesheet" href="./style.css" type='text/css'/>
+     </head>
+     <body>
+        <div className='container'>
+          <h1>Service Requests</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Service</th>
+                <th>Notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              {requests.map((request) => (
+                <tr key={request._id}>
+                  <td>{request.date}</td>
+                  <td>{request.time}</td>
+                  <td>{request.name}</td>
+                  <td>{request.email}</td>
+                  <td>{request.phone}</td>
+                  <td>{request.service}</td>
+                  <td>{request.notes}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+     </body>
+    </>
   );
 }
 
